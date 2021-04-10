2f8c6b2975e264ca7ac77812bf3db63949a4cdd0
owendevscratch <owendevscratch@gmail.com>
2021-04-10 17:23:36 -0400

    Initial commit

---
+++ items.js
@@ -0,0 +1,505 @@
+/// Items:
+let items = {
+    ring: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "MP consumption of all skills -15%": 3,
+            "MP consumption of all skills -30%": 3,
+            "Meso Amount +20%": 3,
+            "Item Drop Rate +20%": 3	
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    pendant: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "MP consumption of all skills -15%": 3,
+            "MP consumption of all skills -30%": 3,
+            "Meso Amount +20%": 3,
+            "Item Drop Rate +20%": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4,
+        }
+    },
+    weapon: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "ATT +12%": 2,
+            "MATT +12%": 2,
+            "Critical Chance +12%": 2,
+            "Damage +12%": 2,
+            "All Stats +9%": 3,
+            "1 ATT per 10 character levels": 2,
+            "1 MATT per 10 character levels": 2,
+            "Ignore Enemy Defense +35%": 2,
+            "Ignore Enemy Defense +40%": 2,
+            "Damage when attacking boss monsters +30%": 2,
+            "Damage when attacking boss monsters +35%": 2,
+            "Damage when attacking boss monsters +40%": 2
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "ATT +9%": 3,
+            "MATT +9%": 3,
+            "Critical Chance +9%": 4,
+            "Damage +9%": 3,
+            "All Stats +6%": 4,
+            "Ignore Enemy Defense +30%": 3,
+            "Damage when attacking boss monsters +20%": 3,
+            "Damage when attacking boss monsters +30%": 2
+        }
+    },
+    belt: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    hat: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "All Skill Levels +2": 3,
+            "All Skill Levels +3": 2,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3,
+            "Skill cooldown -1 second": 3,
+            "Skill cooldown -2 seconds": 2,
+            "Decent Advanced Bless enabled": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "All Skill Levels +1": 4,
+            "All Skill Levels +2": 2,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4,
+            "Decent Mystic Door enabled": 4
+        }
+    },
+    face: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "MP consumption of all skills -15%": 3,
+            "MP consumption of all skills -30%": 3,
+            "Meso Amount +20%": 3,
+            "Item Drop Rate +20%": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    eye: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "MP consumption of all skills -15%": 3,
+            "MP consumption of all skills -30%": 3,
+            "Meso Amount +20%": 3,
+            "Item Drop Rate +20%": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    tops: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "Elemental Resistance +10%": 2,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3,
+            "Invincibility time +3 seconds when hit": 3,
+            "4% chance of being invincible for 7 seconds when hit": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Invincibility time +2 seconds when hit": 4,
+            "2% chance of being invincible for 7 seconds when hit": 4,
+            "30% chance to reflect 50% of damage taken": 4,
+            "30% chance to reflect 70% of damage taken": 2,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    bottom: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "Elemental Resistance +10%": 2,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4,
+            "Decent Hyper Body enabled": 4
+        }
+    },
+    shoes: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3,
+            "Decent Combat Orders enabled": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4,
+            "Decent Haste enabled": 4
+        }
+    },
+    earring: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "MP consumption of all skills -15%": 3,
+            "MP consumption of all skills -30%": 3,
+            "Meso Amount +20%": 3,
+            "Item Drop Rate +20%": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    shoulder: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    gloves: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "Critical Damage +8%": 2,
+            "Critical Damage +8%": 2,
+            "All Stats +9%": 3,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3,
+            "Decent Speed Infusion enabled": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "STR +1 per 10 Character Levels": 1,
+            "DEX +1 per 10 Character Levels": 1,
+            "INT +1 per 10 Character Levels": 1,
+            "LUK +1 per 10 Character Levels": 1,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4,
+            "Decent Sharp Eyes enabled": 4
+        }
+    },
+    emblem: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "ATT +12%": 2,
+            "MATT +12%": 2,
+            "Critical Chance +12%": 2,
+            "Damage +12%": 2,
+            "All Stats +9%": 3,
+            "1 ATT per 10 character levels": 2,
+            "1 MATT per 10 character levels": 2,
+            "Ignore Enemy Defense +35%": 2,
+            "Ignore Enemy Defense +40%": 2
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "ATT +9%": 3,
+            "MATT +9%": 3,
+            "Critical Chance +9%": 4,
+            "Damage +9%": 3,
+            "All Stats +6%": 4,
+            "Ignore Enemy Defense +30%": 3
+        }
+    },
+    secondary: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "ATT +12%": 2,
+            "MATT +12%": 2,
+            "Critical Chance +12%": 2,
+            "Damage +12%": 2,
+            "All Stats +9%": 3,
+            "1 ATT per 10 character levels": 2,
+            "1 MATT per 10 character levels": 2,
+            "Ignore Enemy Defense +35%": 2,
+            "Ignore Enemy Defense +40%": 2,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3,
+            "Damage when attacking boss monsters +30%": 2,
+            "Damage when attacking boss monsters +35%": 2,
+            "Damage when attacking boss monsters +40%": 2
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "ATT +9%": 3,
+            "MATT +9%": 3,
+            "Critical Chance +9%": 4,
+            "Damage +9%": 3,
+            "All Stats +6%": 4,
+            "Ignore Enemy Defense +30%": 3,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Damage when attacking boss monsters +20%": 3,
+            "Damage when attacking boss monsters +30%": 2
+        }
+    },
+    cape: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3,
+            "10% chance to ignore 20% damage when hit": 3,
+            "10% chance to ignore 40% damage when hit": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "5% chance to ignore 20% damage when hit": 4,
+            "5% chance to ignore 40% damage when hit": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    },
+    heart: {
+        prime: {
+            "STR +12%": 4,
+            "DEX +12%": 4,
+            "INT +12%": 4,
+            "LUK +12%": 4,
+            "Max HP +12%": 4,
+            "Max MP +12%": 4,
+            "DEF +12%": 4,
+            "All Stats +9%": 3
+        },
+        secondary: {
+            "STR +9%": 5,
+            "DEX +9%": 5,
+            "INT +9%": 5,
+            "LUK +9%": 5,
+            "Max HP +9%": 6,
+            "Max MP +9%": 6,
+            "DEF +9%": 4,
+            "All Stats +6%": 4,
+            "Increase efficiency of HP recovery items and skills by 30%": 4
+        }
+    }
+}
---
+++ lineGen.js
@@ -0,0 +1,47 @@
+// Setup dropdown
+function setupDropDown(){
+    Object.keys(items).forEach(k => {
+        var itemSelector = document.getElementById("itemType");
+        var option = document.createElement("option");
+        option.text = k;
+        option.value = k;
+        itemSelector.add(option);
+    });
+}
+
+function selectLine(options){
+    var index = Math.floor(Math.random() * Object.keys(options).length);
+    return Object.keys(options)[index];
+}
+
+function generateLines(){
+    // TODO: Certain items cannot have special line combinations (ie: only max 2l boss on wse)
+    var itemType = document.getElementById("itemType").value;
+    var lineOptions = items[itemType];
+    var lines = "";
+    for(let i = 0; i < 6; i++){
+        if(i === 0){
+            // Prime first line
+                lines += `<div>${selectLine(lineOptions["prime"])}</div>`;
+        }else if (i === 2 || i === 4){
+            // 10% of prime line
+            var primeLine = Math.floor(Math.random() * 100) < 10;
+            if(primeLine){
+                lines += `<div>${selectLine(lineOptions["prime"])}</div>`;
+            }else{
+                lines += `<div>${selectLine(lineOptions["secondary"])}</div>`;
+            }
+        }else{
+            // 1% of prime line
+            var primeLine = Math.floor(Math.random() * 100) < 1;
+            if(primeLine){
+                lines += `<div>${selectLine(lineOptions["prime"])}</div>`;
+            }else{
+                lines += `<div>${selectLine(lineOptions["secondary"])}</div>`;
+            }
+        }
+        lines += "\n";
+    }
+    var lineOutput = document.getElementById("lines");
+    lineOutput.innerHTML = lines;
+}
